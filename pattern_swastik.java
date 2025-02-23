9444e1443ae78965d82dc26ede03c16180429cd0
Pratik kumar <148235773+pratik5669git@users.noreply.github.com>
2025-02-23 17:48:24 +0530

    Add files via upload

---
+++ pattern_swastik.java
@@ -0,0 +1,14 @@
+public class pattern_swastik{
+    public static void main(String[] args) {
+        for(int i=1;i<=7;i++){
+            for(int j=1;j<=7;j++){
+                if(j==1 && i<=4 || i==4 || j==4 || j==7 && i>=4 || i==1 && j>=4 || i==7 && j<=4)
+                    System.out.print("*");
+            
+                else
+                    System.out.print(" ");
+              }
+              System.out.println(" ");
+        }
+}
+}
