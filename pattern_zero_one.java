0aeaedee081429be90961b9578d797dec16ee65a
Pratik kumar <148235773+pratik5669git@users.noreply.github.com>
2025-02-24 10:05:26 +0530

    Update pattern_zero_one.java

--- pattern_zero_one.java
+++ pattern_zero_one.java
@@ -1,9 +1,9 @@
 public class pattern_zero_one {
     public static void main(String[] args) {
         for(int i=1;i<=5;i++){
+            int k=1;
             for(int j=1;j<=5;j++){
-                System.out.print(i);
-                System.out.print(j);
+                System.out.print(k++%2);
             }
               System.out.println(" ");
         }
