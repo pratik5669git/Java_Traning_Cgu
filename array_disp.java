4a7ddedf22f94c0fed39e5b180bd56608d2a16e0
Pratik kumar <148235773+pratik5669git@users.noreply.github.com>
2025-03-07 23:28:15 +0530

    Add files via upload

---
+++ array_disp.java
@@ -0,0 +1,9 @@
+public class array_disp {
+    public static void main(String[] args) {
+    int arr[]={1,2,3,4,5};
+    int n=arr[arr.length-1];
+    for(int i=1;i<=10;i++){
+        System.out.println(n+"x"+i+"="+ n*i);
+    }
+    }
+}
